e11a95f8699e5cfa4731703e21e1c8f41315124c
fucchy <fucchy0907@gmail.com>
2016-02-15 01:41:47 +0900

    画像表示した(DrawGraph関数使用)

--- chap3/main.cpp
+++ chap3/main.cpp
@@ -5,9 +5,11 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpC, int nC) {
 	ChangeWindowMode(TRUE);
 	//DXライブラリ初期化
 	if (DxLib_Init() == -1) return -1;
-	//ボックスを描画
-	int col = GetColor(255, 255, 0);
-	DrawBox(520, 360, 640, 480, col, TRUE);
+
+	//画像を読み込み
+	int ghandle = LoadGraph("media\\smp1_chara01.png");
+	DrawGraph(0, 0, ghandle, TRUE);
+
 	//待機
 	WaitKey();
 	//DXライブラリの終了処理
