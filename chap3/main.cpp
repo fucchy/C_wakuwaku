12b8114511236050c1f0936f42460aab7e3dad0f
fucchy <fucchy0907@gmail.com>
2016-02-15 01:07:46 +0900

    DrawBox関数の色指定の引数を、直接指定からRGBでの指定に変更(GetColor関数を使ってRGB形式の色指定を整数値に変更)

--- chap3/main.cpp
+++ chap3/main.cpp
@@ -6,7 +6,8 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpC, int nC) {
 	//DXライブラリ初期化
 	if (DxLib_Init() == -1) return -1;
 	//ボックスを描画
-	DrawBox(520, 360, 640, 480, 0x00FFFF, TRUE);
+	int col = GetColor(255, 255, 0);
+	DrawBox(520, 360, 640, 480, col, TRUE);
 	//待機
 	WaitKey();
 	//DXライブラリの終了処理
