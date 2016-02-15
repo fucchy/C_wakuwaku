5c34f62db690c221683f226a6c59bd6d15ae8832
fucchy <fucchy0907@gmail.com>
2016-02-16 01:48:49 +0900

    キー入力に応じてキャラクターを移動させる(GetJoypadInputState関数)

--- chap3/main.cpp
+++ chap3/main.cpp
@@ -8,13 +8,18 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpC, int nC) {
 
 	//画像を読み込み
 	int ghandle = LoadGraph("media\\smp1_chara01.png");
-	int x = 0;
+	int x = 0, y = 0;
 
 	SetDrawScreen(DX_SCREEN_BACK); //ウラ画面を描画対象にする
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
 		ClearDrawScreen(); //画面消去
-		if (x <= 320) x += 1;
-		DrawGraph(x, 224, ghandle, TRUE);
+		//自キャラ移動
+		int key = GetJoypadInputState(DX_INPUT_KEY_PAD1); //キーのチェック
+		if (key & PAD_INPUT_UP)		y -= 4;
+		if (key & PAD_INPUT_DOWN)	y += 4;
+		if (key & PAD_INPUT_LEFT)	x -= 4;
+		if (key & PAD_INPUT_RIGHT)	x += 4;
+		DrawGraph(x, y, ghandle, TRUE);
 		ScreenFlip(); //画面入れ替え
 	}
 
