c1a58625b2d6b66cdb1d4125b659cf0f67975ff0
fucchy <fucchy0907@gmail.com>
2016-02-16 22:08:04 +0900

    リアルタイムゲームの為に、タイミングを調整する処理を加えた

--- chap3/main.cpp
+++ chap3/main.cpp
@@ -1,5 +1,10 @@
 #include <DxLib.h>
 
+//グローバル変数
+//時間計測用変数
+int g_lasttime = 0; //直前の計測時間
+float g_frametime = 0; //1ループにかかった時間
+
 int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpC, int nC) {
 	//ウィンドウズモードにする
 	ChangeWindowMode(TRUE);
@@ -8,18 +13,26 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpC, int nC) {
 
 	//画像を読み込み
 	int ghandle = LoadGraph("media\\smp1_chara01.png");
-	int x = 0, y = 0;
+	float x = 0, y = 0;
 
 	SetDrawScreen(DX_SCREEN_BACK); //ウラ画面を描画対象にする
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
+		//1ループにかかった時間を計測
+		int curtime = GetNowCount();
+		g_frametime = (float)(curtime - g_lasttime) / 1000.0f;
+		g_lasttime = curtime;
+
 		ClearDrawScreen(); //画面消去
 		//自キャラ移動
 		int key = GetJoypadInputState(DX_INPUT_KEY_PAD1); //キーのチェック
-		if (key & PAD_INPUT_UP)		y -= 4;
-		if (key & PAD_INPUT_DOWN)	y += 4;
-		if (key & PAD_INPUT_LEFT)	x -= 4;
-		if (key & PAD_INPUT_RIGHT)	x += 4;
+		float mv = 80.0f * g_frametime; //移動量計算
+		if (key & PAD_INPUT_UP)		y -= mv;
+		if (key & PAD_INPUT_DOWN)	y += mv;
+		if (key & PAD_INPUT_LEFT)	x -= mv;
+		if (key & PAD_INPUT_RIGHT)	x += mv;
 		DrawGraph(x, y, ghandle, TRUE);
+		clsDx();
+		printfDx("%d : %f : %f", g_lasttime, g_frametime, mv);
 		ScreenFlip(); //画面入れ替え
 	}
 
